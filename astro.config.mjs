83eb229eb96d6bfa88b0896aa87a1f8cf0d0a50a
ethan_talinge <ethan.leneindre9@gmail.com>
2026-06-30 22:26:22 +0200

    feat: Add internationalization

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,15 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    i18n: {
+        locales: [
+            "en",
+            "fr"
+        ],
+        defaultLocale: "fr",
+        routing: {
+            prefixDefaultLocale: true
+        }
+    }
+});
