3d6bc41dea74805a5f12d9c35c6d41a9e90de34a
ethan_talinge <ethan.leneindre9@gmail.com>
2026-06-30 21:38:13 +0200

    Rebuild my portfolio Rebuild from NextJS to Astro + complete redesign of portfolio

--- lib/age.ts
+++
@@ -1,9 +0,0 @@
-const birth = new Date("2004-07-16");
-const now = new Date();
-const age =
-    now.getMonth() > birth.getMonth() ||
-        (now.getMonth() === birth.getMonth() && now.getDate() >= birth.getDate())
-        ? now.getFullYear() - birth.getFullYear()
-        : now.getFullYear() - birth.getFullYear() - 1;
-
-export default age;
